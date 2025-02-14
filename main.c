ca72b9d28c4777b1de59b8e154b175aa4c4fa6d6
Ilia Popovsky <popovskyilia@gmail.com>
2025-02-14 17:10:17 +0300

    first version project1

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
 
-int main()
+#define NUMBER_PORT 96
+#define SIZE_BUF 1024
+#define NAME_HOST "telehack.com"
+#define NAME_SERVICE "telnet"
+
+int main(int argc, char **argv)
 {
     printf("Hello world!\n");
     return 0;
