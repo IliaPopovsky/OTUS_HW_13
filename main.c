82ad04625c67d1860fd7f7b150f553db5007d961
Ilia Popovsky <popovskyilia@gmail.com>
2025-02-14 19:53:56 +0300

    first version project2

--- main.c
+++ main.c
@@ -15,6 +15,23 @@
 
 int main(int argc, char **argv)
 {
+
+    char buf[SIZE_BUF] = { 0 };
+    int fd = 0;;
+    struct addrinfo *result;
+    struct addrinfo hints = {0};
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0;
+    hints.ai_protocol = IPPROTO_TCP;
+
+
+    int info = getaddrinfo(NAME_HOST, NAME_SERVICE, &hints, &result);
+    if (info != 0) {
+	printf("getaddinfo error\n");
+       exit(0);
+    }
+
     printf("Hello world!\n");
     return 0;
 }
